9c08a9803d7b4a7862d0a86bc97cae78296f1a8f
Dhruv Parashar <dhruvparashar246@gmail.com>
2026-01-19 03:09:18 -0500

    Added Resume Link

--- components/header.tsx
+++ components/header.tsx
@@ -49,7 +49,7 @@ export function Header() {
           ))}
           <li>
             <a
-              href="/Dhruv_Parashar_Resume.pdf"
+              href="https://www.dropbox.com/scl/fi/ln1b9lcu8s6v3fd38v99z/Dhruv_Parashar_Resume.pdf?rlkey=lit2984w4qdds5mrmz6loosek&st=lddcnmc6&dl=0"
               target="_blank"
               rel="noopener noreferrer"
               className="rounded-md border border-primary px-4 py-2 text-sm text-primary transition-all hover:bg-primary/10"
